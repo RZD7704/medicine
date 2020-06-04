d452da471b5b4667a1d452d56495076bb237936d
Artem <artemius2014poltava@gmail.com>
2020-06-04 11:54:49 +0300

    Add slider bg

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@
 (function($){
 	$(document).ready(function() {
 		// Code
-
+		// Slider
+		$('.sl').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			// autoplay: false,
+			// autoplaySpeed: 2000,
+			dots: true
+		});
 	});
 })(jQuery);
