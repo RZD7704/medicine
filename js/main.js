3ee13ad90271efc68d75867cc279da1c6de79315
Artem <artemius2014poltava@gmail.com>
2020-06-05 17:30:20 +0300

    Add changes

--- js/main.js
+++ js/main.js
@@ -7,9 +7,26 @@
 		$('.sl').slick({
 			slidesToShow: 1,
 			slidesToScroll: 1,
-			// autoplay: false,
-			// autoplaySpeed: 2000,
+			autoplay: true,
+			autoplaySpeed: 7000,
 			dots: true
+		});
+
+		$('.doctors__sl').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			// autoplay: true,
+			// autoplaySpeed: 2000,
+			dots: true,
+			dotsClass: 'slick-dots slider__dots doctors__sl--dots',
+			responsive: [
+				{
+				  breakpoint: 991,
+				  settings: {
+				   slidesToShow: 1,
+				   slidesToScroll: 1	
+				 }
+				}] 
 		});
 	});
 })(jQuery);
